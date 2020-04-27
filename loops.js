cc90ca45eb93900fe4849c986be4b905317e051f
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-04-27 18:38:03 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,31 @@
+function forLoop(array)
+{
+  for(let i = 1, i < array.length, i +=1)
+  {
+    array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
+  }
+}
+
+function whileLoop(n)
+{
+  while(n >= 0)
+  {
+    console.log(n)
+    n -= 1
+  }
+}
+
+function incrementVariable() 
+{
+  i = i + 1;
+  return i;
+}
+
+
+function doWhileLoop(num)
+{
+  let i = 0
+  do {
+    console.log("I run once regardless.")
+  } while (incrementVariable(i) < num );
+}
